e2b2b2d2c38a45b074a4566abefee00fa81c690d
Mateo Fiorotto <mateofiorotto2004@gmail.com>
2025-05-23 15:22:18 -0300

    semana 10

--- Semana09/desafio09/src/components/TodoList.jsx
+++ Semana09/desafio09/src/components/TodoList.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function TodoList(props) {
     return (
-            <ul className='list-unstyled todo-list row justify-content-center align-items-center text-center'>
+            <ul className='pt-5 pb-5 list-unstyled todo-list row justify-content-center align-items-center text-center'>
                 {props.children}
             </ul>
     )
